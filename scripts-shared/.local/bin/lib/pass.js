5fbe5a945868b664f8d01018d35b379f92acd07f
Avi Areman <avi.areman@gmail.com>
2023-01-10 10:43:33 -0500

    feat: add fallback for pass.js to pull from env
    This is needed for dev containers that don't have your pass store

--- scripts-shared/.local/bin/lib/pass.js
+++ scripts-shared/.local/bin/lib/pass.js
@@ -1,4 +1,4 @@
 const exec = require("child_process").execSync
 
-exports.passGet = (name) => exec(`pass ${name}`).toString().replace("\n", "")
+exports.passGet = (name) => exec(`pass ${name}`).toString().replace("\n", "") || process.ENV[name.replaceAll(" ", "_").toUpperCase()]
 exports.passStore = (name, value) => exec(`echo "${value}" | pass insert --echo ${name}`)
