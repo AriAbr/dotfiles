d89eea7c9bfc3fc0b4e1e71e010b042459531552
Avi Areman <avi.areman@gmail.com>
2023-08-16 12:32:50 -0400

    fix: add try/catch to pass.js
    Also fix syntax usage of `process.ENV` ~> `process.env`

--- stowables/scripts-shared/.local/bin/lib/pass.js
+++ stowables/scripts-shared/.local/bin/lib/pass.js
@@ -1,4 +1,10 @@
 const exec = require("child_process").execSync
 
-exports.passGet = (name) => exec(`pass ${name}`).toString().replace("\n", "") || process.ENV[name.replaceAll(" ", "_").toUpperCase()]
+exports.passGet = (name) => {
+    try {
+        return exec(`pass ${name}`).toString().replace("\n", "")
+    } catch {
+        return process.env[name.replaceAll(" ", "_").toUpperCase()]
+    }
+};
 exports.passStore = (name, value) => exec(`echo "${value}" | pass insert --echo ${name}`)
