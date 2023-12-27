4f9766b16ea4a07775be5a6c2329a9c317f77cc5
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-12-27 12:35:22 +0200

    feat: Add to post-install

--- ari-post-install.sh
+++ ari-post-install.sh
@@ -3,11 +3,21 @@
 # python3
 sudo apt install python3-pip python-is-python3 -y
 
+# autorandr
+sudo apt install autorandr -y
+
+# nala
+sudo apt install nala -y
+
 # i3
 sudo apt install i3 brightnessctl gnome-screensaver feh -y
 mkdir ~/.config/i3
 git clone git@github.com:AriAbr/i3-setup.git ~/.config/i3
 sudo apt install i3status suckless-tools i3blocks rofi arandr -y
+
+# i3-gnome post install
+sudo apt install -y gnome-flashback gnome-power-manager
+gsettings set org.gnome.gnome-flashback desktop false
 
 # i3blocks
 sudo apt install fonts-font-awesome acpi light -y
