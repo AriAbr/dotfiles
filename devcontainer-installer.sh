66b49452ee96235a5ac7f076c4d368887d781eb3
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-01-07 22:56:40 +0200

    fix: Remove .gitconfig before stowing

--- devcontainer-installer.sh
+++ devcontainer-installer.sh
@@ -50,8 +50,9 @@ center "Stowing Dotfiles"
 center "Install node dependencies for scripts"
 spinner "(cd stowables/scripts-shared/.local/bin && yarn install)"
 
-# Delete .bashrc to prevent stow error
+# Delete files to prevent stow error
 rm ~/.bashrc
+rm ~/.gitconfig
 
 spinner stow -v -R -d stowables/ -t "$HOME" \
 bash \
