dff07415a96d466c2f48f1deca851b3b44c88f13
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-01-08 18:25:39 +0200

    feat: Disable ipv6

--- ari-post-install.sh
+++ ari-post-install.sh
@@ -39,3 +39,19 @@ git clone https://github.com/hastinbe/i3-volume.git ~/.config/i3-volume
 
 # rofi theme
 rofi-theme-selector
+
+# disable ipv6
+disable_ipv6_sysctl() {
+  cat <<EOL | sudo tee -a /etc/sysctl.conf > /dev/null
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOL
+}
+
+activate_sysctl() {
+    sudo sysctl -p
+}
+
+disable_ipv6_sysctl
+activate_sysctl
